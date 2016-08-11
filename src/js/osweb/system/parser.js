3ea22216399993e9ad27a7d5e0102fcf890caf81
p106774 <p106774@WKS010632.workspace.rug.nl>
2016-08-11 16:06:15 +0200

    - Experimenting with the parser

--- src/js/osweb/system/parser.js
+++ src/js/osweb/system/parser.js
@@ -49,6 +49,98 @@
 
     // Definition of private methods - node processing.
 
+    parser._node_array_expression = function()
+    {
+        // Initialize node properties. 
+        this._current_node.index = (typeof this._current_node.index === 'undefined') ? 0 : this._current_node.index;
+        this._current_node.status = (typeof this._current_node.status === 'undefined') ? 0 : this._current_node.status;
+
+        // Process the current status.
+        switch (this._current_node.status) {
+            case 0:
+                // Process arguments.
+                if (this._current_node.index < this._current_node.elements.length) {
+                    // Set current node to next node in list.
+                    this._current_node.index++;
+                    this._current_node.elements[this._current_node.index - 1].parent = this._current_node;
+                    this._current_node = this._current_node.elements[this._current_node.index - 1];
+
+                    // Return to the node processessor.
+                    this._process_node();
+                } else {
+                    // Set parent node.
+                    this._current_node.status = 1;
+
+                    // Return to the node processessor.
+                    this._process_node();
+                }
+            break;
+            case 1:
+                // Create and set the returnvalue.
+                returnvalue = new Array();
+                if (this._current_node.index > 0) {
+                    for (var i=0;i < this._current_node.index;i++) {
+                        returnvalue.push(this._current_node['returnvalue' + String(i)]);
+                    }
+                }   
+                this._set_return_value(this._current_node.parent, returnvalue);
+
+                // Reset status of node and return to the parten node to process.
+                this._current_node.index = 0;
+                this._current_node.status = 0;
+                this._current_node = this._current_node.parent;
+                this._process_node();
+            break;    
+        }    
+    };
+
+    parser._node_assignment_expression = function() {
+        // Initialize status property.
+        this._current_node.status = (typeof this._current_node.status === 'undefined') ? 0 : this._current_node.status;
+
+        // Process the current status.
+        switch (this._current_node.status) {
+            case 0:
+                // process right expression;
+                this._current_node.status = 1;
+                this._current_node.right.parent = this._current_node;
+                this._current_node = this._current_node.right;
+
+                // Return to the node processessor.
+                this._process_node();
+            break;    
+            case 1:
+                // process right expression;
+                this._current_node.status = 2;
+                this._current_node.left.parent = this._current_node;
+                this._current_node = this._current_node.left;
+
+                // Return to the node processessor.
+                this._process_node();
+            break;    
+            case 2:
+                // Perform the assignment.
+                switch (typeof this._current_node.returnvalue1) {
+                    case 'object': 
+                        switch (this._current_node.returnvalue1.obj) {
+                            case 'var': 
+                                osweb.runner.experiment.vars[this._current_node.returnvalue1.prop] = this._current_node.returnvalue0;
+                                console.log(osweb.runner.experiment.vars);
+                            break
+                    }    
+                    break;
+                }    
+                
+                // Set parent node.
+                this._current_node.status = 3;
+                this._current_node = this._current_node.parent;
+
+                // Return to the node processessor.
+                this._process_node();
+            break;
+        }          
+    }; 
+
     parser._node_binary_expression = function() {
         // Initialize status property.
         this._current_node.status = (typeof this._current_node.status === 'undefined') ? 0 : this._current_node.status;
@@ -92,7 +184,37 @@
                     case '-':
                         // Process call - check for blocking methods.
                         this._set_return_value(this._current_node.parent, left - right);
-                        break;
+                    break;
+                    case '/':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left / right);
+                    break;
+                    case '==':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left === right);
+                    break;
+                    case '!=':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left !== right);
+                    break;
+                    case '>':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left > right);
+                    break;
+                    case '<':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left < right);
+                    break;
+                    case '>=':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left >= right);
+                    break;
+                    case '<=':
+                        // Process call - check for blocking methods.
+                        this._set_return_value(this._current_node.parent, left <= right);
+                    break;
+                    default:
+                        console.log('script error: no valid binary operator.');
                 }
 
                 // Set parent node.
@@ -101,7 +223,7 @@
 
                 // Return to the node processessor.
                 this._process_node();
-                break;
+            break;
         }
     };
 
@@ -132,7 +254,7 @@
                 }
                 break;
             case 1:
-                // Set parent node.
+                // Process method.
                 this._current_node.status = 2;
                 this._current_node.callee.parent = this._current_node;
                 this._current_node = this._current_node.callee;
@@ -157,12 +279,24 @@
                 // Select the type of call to process
                 var callee = this._current_node['returnvalue' + String(this._current_node.arguments.length)];
                 var returnvalue = null;
+                
                 if (callee.type == 'function') {
-                    // function call
-                    returnvalue = window[callee.obj].apply(null, tmp_arguments);
-
-                    // Process call - check for blocking methods.
-                    this._current_node.parent['returnvalue' + String(this._current_node.arguments.length)] = returnvalue;
+                    // process special expression methods
+                    switch (callee.obj) { 
+                        case 'add': 
+                            returnvalue = tmp_arguments[1] + tmp_arguments[0];
+                        break;
+                        case 'multiply': 
+                            returnvalue = tmp_arguments[1] * tmp_arguments[0];
+                        break;    
+                        default:
+                            // function call
+                            returnvalue = window[callee.obj].apply(null, tmp_arguments);
+                        break;        
+                    }
+                    
+                    // Set return value for the parent node.     
+                    this._set_return_value(this._current_node.parent, returnvalue);
 
                     // Return to the node processessor.
                     this._process_node();
@@ -205,6 +339,14 @@
         }
     };
 
+    parser._node_empty_statement = function() {
+        // Process empty statement, return to parent.
+        this._current_node = this._current_node.parent;
+
+        // Return to the node processessor.
+        this._process_node();
+    };
+
     parser._node_expression_statement = function() {
         // Initialize status property.
         this._current_node.status = (typeof this._current_node.status === 'undefined') ? 0 : this._current_node.status;
@@ -232,8 +374,8 @@
     };
 
     parser._node_identifier = function() {
-        // Return function result.
-        if (typeof window[this._current_node.name] === 'undefined') {
+        // Check for pre-defined global classes (var, exp e.d.) 
+        if ((this._current_node.name in ['items','pool','responses','var'] === false) && (typeof window[this._current_node.name] === 'undefined')) {
             // Item is undefined, create it without value/type definition.
             window[this._current_node.name] = null;
         }
@@ -311,6 +453,86 @@
                 break;
         }
     };
+    
+    parser._node_new_expression = function() {
+        // Initialize status and index properties.
+        this._current_node.status = (typeof this._current_node.status === 'undefined') ? 0 : this._current_node.status;
+        this._current_node.index = (typeof this._current_node.index === 'undefined') ? 0 : this._current_node.index;
+
+        // Process the current status.
+        switch (this._current_node.status) {
+            case 0:
+                // Process method.
+                this._current_node.status = 1;
+                this._current_node.callee.parent = this._current_node;
+                this._current_node = this._current_node.callee;
+
+                // Return to the node processessor.
+                this._process_node();
+            break;
+            case 1:
+                // Process arguments.
+                if (this._current_node.index < this._current_node.arguments.length) {
+                    // Set current node to next node in list.
+                    this._current_node.index++;
+                    this._current_node.arguments[this._current_node.index - 1].parent = this._current_node;
+                    this._current_node = this._current_node.arguments[this._current_node.index - 1];
+
+                    // Return to the node processessor.
+                    this._process_node();
+                } 
+                else {
+                    // Set parent node.
+                    this._current_node.status = 2;
+                
+                    // Return to the node processessor.
+                    this._process_node();
+                }
+            break;
+            case 2:
+                var returnvalue;
+                var index = 0;
+                
+                // Create the desired expression.
+                switch (this._current_node.returnvalue0.obj)
+                {
+                    case 'dict': 
+                        returnvalue = {};
+                        if (this._current_node.index > 0) {
+                            for (var i=1;i <= this._current_node.index;i++) {
+                                returnvalue[this._current_node['returnvalue' + String(i)][0]] = this._current_node['returnvalue' + String(i)][1];
+                            }            
+                        }
+                    break;       
+                    case 'list': 
+                        returnvalue = new Array();
+                        if (this._current_node.index > 0) {
+                            for (var i=1;i <= this._current_node.index;i++) {
+                                returnvalue.push(this._current_node['returnvalue' + String(i)]);
+                            }
+                        }   
+                    break;       
+                    case 'tuple':
+                        returnvalue = new Array();
+                        if (this._current_node.index > 0) {
+                            for (var i=1;i <= this._current_node.index;i++) {
+                                returnvalue.push(this._current_node['returnvalue' + String(i)]);
+                            }
+                        }   
+                    break;
+                }  
+                
+                // Set return value for the parent node.     
+                this._set_return_value(this._current_node.parent, returnvalue);
+                
+                // Reset status of node and return to the parten node to process.
+                this._current_node.index = 0;
+                this._current_node.status = 0;
+                this._current_node = this._current_node.parent;
+                this._process_node();
+             break;
+        }    
+    }; 
 
     parser._node_program = function() {
         // Initialize index counter only the fitst time.
@@ -436,11 +658,20 @@
 
         // Select the type of node to process
         switch (this._current_node.type) {
+            case 'ArrayExpression':
+                this._node_array_expression();
+                break;    
+            case 'AssignmentExpression':
+                this._node_assignment_expression();
+                break;    
             case 'BinaryExpression':
                 this._node_binary_expression();
                 break;
             case 'CallExpression':
                 this._node_call_expression();
+                break;
+            case 'EmptyStatement':
+                this._node_empty_statement();
                 break;
             case 'ExpressionStatement':
                 this._node_expression_statement();
@@ -453,6 +684,9 @@
                 break;
             case 'MemberExpression':
                 this._node_member_expression();
+                break;
+            case 'NewExpression':
+                this._node_new_expression();
                 break;
             case 'Program':
                 this._node_program();
@@ -481,6 +715,8 @@
         this._current_node = ast_tree;
         this._current_node.parent = null;
         this._status = 1;
+
+        console.log(this._current_node);
 
         // Process the nodes. 
         osweb.parser._process_node();
