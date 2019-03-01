d3181b6ebd41885a04e51a2cee39e74b545bf754
Daniel Schreij <dschreij@gmail.com>
2019-03-01 13:06:40 +0100

    Fix jest tests on Windows

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+export default function babelConfig (api) {
+  api.cache(true)
+  return {
+    'plugins': [
+      'lodash',
+      '@babel/plugin-transform-runtime'
+    ],
+    'presets': [
+      ['@babel/preset-env', {
+        'modules': false
+      }]
+    ],
+    'env': {
+      'test': {
+        'presets': [
+          '@babel/preset-env',
+          '@babel/preset-react'
+        ]
+      }
+    }
+  }
+}
