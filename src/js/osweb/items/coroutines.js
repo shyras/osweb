c2b0b8e866485cf71cea5fc0af10e0dd2b677414
Daniel Schreij <dschreij@gmail.com>
2019-03-05 09:53:10 +0100

    Progress...

--- src/js/osweb/items/coroutines.js
+++ src/js/osweb/items/coroutines.js
@@ -29,7 +29,7 @@ export default class Coroutines extends Item {
         const task = {
           item_name: arglist.shift(),
           start_time: kwdict.start || 0,
-          end_time: kwdict.end || null,
+          end_time: kwdict.end || 0,
           run_if: kwdict.runif || 'always'
         }
         this.schedule.push(task)
@@ -38,29 +38,31 @@ export default class Coroutines extends Item {
   }
 
   prepare () {
-    super.prepare()
-    this.schedule = this.schedule.map(task => {
-      const item = this._runner._itemStore._items[task.item_name]
-      if (item) {
-        const msg = `Coroutines '${name}' - could not find item: ${this.item_name}`
+    console.log('Starting prepare')
+    this.schedule = this.schedule.map(taskParams => {
+      const item = this._runner._itemStore._items[taskParams.item_name]
+      if (!item) {
+        const msg = `Coroutines '${name}' - could not find item: ${taskParams.item_name}`
         this._runner._debugger.addError(msg)
         throw new Error(msg)
       }
-      return new Task(item, ...task)
+      return new Task(item, ...Object.values(taskParams))
     })
+    console.log('Finished prepare')
+    super.prepare()
   }
 
   run () {
+    console.log('Starting run phase')
     super.run()
     // Prepare all tasks
     for (const task of this.schedule) {
       this._runner._itemStore.prepare(task.item_name, this)
     }
     // Launch all tasks and wrap them in the coroutine helper
-    this.schedule = this.schedule.map(task => {
-      task.step = this._coroutine(task.item.coroutine)
-      return task
-    })
+    for (let task in this.schedule) {
+      task.launch()
+    }
 
     this.schedule = sortBy(this.schedule, 'start_time')
 
@@ -76,6 +78,26 @@ export default class Coroutines extends Item {
       dt = performance.now() - t0
     }
   }
+}
+
+class Task {
+  UNINITIALISED = 0
+  RUNNING = 1
+  ABORTED = 2
+  FINISHED = 3
+
+  constructor (item, item_name, start_time, end_time, run_if) {
+    this.item = item
+    this.item_name = item_name
+    this.start_time = start_time
+    this.end_time = end_time
+    this.condition = run_if
+    this.step = () => { throw new Error('Task has not been initialized') }
+  }
+
+  launch () {
+    this.step = this._coroutine(this.item.coroutine)
+  }
 
   _coroutine (f) {
     const o = f() // instantiate the coroutine
@@ -83,15 +105,5 @@ export default class Coroutines extends Item {
     return function (x) {
       o.next(x)
     }
-  }
-}
-
-class Task {
-  construct (item, item_name, start_time, end_time, run_if) {
-    this.item = item
-    this.item_name = item_name
-    this.start_time = start_time
-    this.end_time = end_time
-    this.condition = run_if
   }
 }
