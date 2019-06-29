ae06c0367a7e1e44245ec30a373a05b1442e5187
Daniel Schreij <dschreij@gmail.com>
2019-06-29 12:12:09 +0200

    Make Canvas vailable in js workspace

--- src/js/osweb/classes/javascript_workspace.js
+++ src/js/osweb/classes/javascript_workspace.js
@@ -1,10 +1,11 @@
+import CanvasClass from '../backends/canvas'
+
 /**
  * A proxy handler for the VarStore that maps properties onto calls to
  * VarStore.get(), so that variables are automatically evaluated, just like
  * in the OpenSesame `var` API.
  */
 class VarStoreHandler {
-
   get (target, prop) {
     // The VarStore sets a property on itself to bypass this proxy. This
     // avoids feedback loops when the VarStore tries to get a variable without
@@ -16,16 +17,13 @@ class VarStoreHandler {
       ? target.get(prop, null, true, null, false)
       : target[prop]
   }
-
 }
-
 
 /**
  * A workspace for executing inline JavaScript code. For now, the workspace is
  * not persistent, and only exposes the vars object.
  */
 export default class JavaScriptWorkspace {
-
   /**
      * Create a JavaScript workspace.
      * @param {Object} experiment - The experiment item to which the item belongs.
@@ -41,6 +39,7 @@ export default class JavaScriptWorkspace {
      */
   _eval (js) {
     let vars = this.vars_proxy
+    let Canvas = () => CanvasClass(this.experiment)
     eval(js)
   }
 }
